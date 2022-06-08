47cb2f81cbfdd2c0ba9bb90a34222c4b101262ec
Wesley M <wesleymdeandrade@gmail.com>
2022-06-08 16:42:26 -0300

    Adds files to run application on Docker

--- src/test/java/br/com/fta/ApplicationTests.java
+++ src/test/java/br/com/fta/ApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.fta;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package br.com.fta;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
