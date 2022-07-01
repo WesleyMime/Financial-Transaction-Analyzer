e936e7da14344b70e229423aa99fdc16b946ebe1
Wesley M <wesleymdeandrade@gmail.com>
2022-07-01 20:33:30 -0300

    Shared package

---
+++ src/main/java/br/com/fta/shared/security/AuthUserDetailService.java
@@ -0,0 +1,44 @@
+package br.com.fta.shared.security;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.fta.user.User;
+import br.com.fta.user.UserRepository;
+
+@Service
+public class AuthUserDetailService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<User> userOptional = userRepository.findByEmail(username);
+		if (userOptional.isEmpty()) {
+			
+			throw new UsernameNotFoundException("Email not registered.");
+		}
+		User user = userOptional.get();
+		
+		Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+
+        user.getAuthorities()
+          .forEach(role -> {
+              grantedAuthorities.add(new SimpleGrantedAuthority(role.getAuthority()));
+          });
+		
+		return new org.springframework.security.core.userdetails.User(
+				user.getUsername(), user.getPassword(), grantedAuthorities);
+	}
+
+}
