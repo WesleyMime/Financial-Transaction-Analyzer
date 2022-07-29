8d5c82afc4c77c97d6b68961d0c5018379971d7b
Wesley M <wesleymdeandrade@gmail.com>
2022-07-29 15:52:21 -0300

    Circuit Breaker

--- fta/src/main/java/br/com/fta/transaction/application/FraudClient.java
+++ fta/src/main/java/br/com/fta/transaction/application/FraudClient.java
@@ -4,14 +4,26 @@ import br.com.fta.transaction.domain.Frauds;
 import br.com.fta.transaction.domain.Transaction;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Set;
 
-@FeignClient("fraud-service")
+@FeignClient(value = "fraud-service", fallback = FraudClient.FraudClientFallback.class)
 @Profile({"dev", "default", "prod"})
 public interface FraudClient {
 
     @PostMapping("/detect-frauds")
-    public Frauds detectFrauds(List<Transaction> list);
+    Frauds detectFrauds(List<Transaction> list);
+
+    @Component
+    @Profile({"dev", "default", "prod"})
+    class FraudClientFallback implements FraudClient {
+
+        @Override
+        public Frauds detectFrauds(List<Transaction> list) {
+            return new Frauds(List.of(), Set.of(), Set.of());
+        }
+    }
 }
--- fta/src/main/java/br/com/fta/transaction/application/GeneratorClient.java
+++ fta/src/main/java/br/com/fta/transaction/application/GeneratorClient.java
@@ -1,17 +1,35 @@
 package br.com.fta.transaction.application;
 
+import br.com.fta.transaction.domain.BankAccount;
 import br.com.fta.transaction.domain.Transaction;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-@FeignClient("transactions-generator-service")
+@FeignClient(value = "transactions-generator-service", fallback = GeneratorClient.GeneratorClientFallback.class)
 @Profile({"dev", "default", "prod"})
 public interface GeneratorClient {
 
     @GetMapping("/generate-transactions")
     List<Transaction> generateTransactions();
+
+
+    @Component
+    @Profile({"dev", "default", "prod"})
+    class GeneratorClientFallback implements GeneratorClient {
+
+        @Override
+        public List<Transaction> generateTransactions() {
+            return List.of(new Transaction(
+                    new BankAccount("Foo", "Bar", "123"),
+                    new BankAccount("Bar", "Foo", "456"),
+                    "10523", LocalDateTime.now()
+            ));
+        }
+    }
 
 }
