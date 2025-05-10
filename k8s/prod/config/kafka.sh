2cc9bcf9a67e63afcd5ff0c583bd78c404ec3b59
Wesley M. <55067868+WesleyMime@users.noreply.github.com>
2025-05-10 06:30:45 -0300

    build: fix k8s configs for deploy

--- k8s/prod/config/kafka.sh
+++ k8s/prod/config/kafka.sh
@@ -1,4 +1,4 @@
 helm install kafka \
-    --set persistence.enabled=false,zookeeper.persistence.enabled=false \
+    --set persistence.enabled=false,zookeeper.persistence.enabled=false,listeners.client.protocol=plaintext,listeners.controller.protocol=plaintext,listeners.interbroker.protocol=plaintext \
     oci://registry-1.docker.io/bitnamicharts/kafka
 sleep 15
