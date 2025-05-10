4cb81ed2ac27fc4cfd89e2620e88d5d63edb28ad
Wesley M. <55067868+WesleyMime@users.noreply.github.com>
2025-05-10 19:47:47 -0300

    refactor: send e-mail using custom domain

--- email-service/src/main/java/br/com/fta/email/model/EmailConfig.java
+++ email-service/src/main/java/br/com/fta/email/model/EmailConfig.java
@@ -1,5 +1,6 @@
 package br.com.fta.email.model;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -8,18 +9,26 @@ import java.util.Properties;
 
 public class EmailConfig {
 
+	@Value("${spring.mail.username}")
+	private String EMAIL_USERNAME;
+
+	@Value("${spring.mail.password}")
+	private String EMAIL_PASSWORD;
+
 	@Bean
 	public JavaMailSender getJavaMailSender() {
 	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 	    mailSender.setHost("smtp.gmail.com");
 	    mailSender.setPort(587);
-	    
+		mailSender.setUsername(EMAIL_USERNAME);
+		mailSender.setPassword(EMAIL_PASSWORD);
+
 	    Properties props = mailSender.getJavaMailProperties();
 	    props.put("mail.transport.protocol", "smtp");
 	    props.put("mail.smtp.auth", "true");
 	    props.put("mail.smtp.starttls.enable", "true");
 	    props.put("mail.debug", "true");
-	    
-	    return mailSender;
+
+		return mailSender;
 	}
 }
